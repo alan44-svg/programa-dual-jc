9578b56814193c60ba41e1de18aa53e5ba00cd3f
Alan Iglesias <iglesiasalan44@gmail.com>
2026-07-16 12:20:13 -0600

    se agrega un boton que al dar click da un saludo

---
+++ proyectoweb/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("btnSaludar").addEventListener("click", function () {
+        alert("¡Bienvenido a JavaScript!");
+    });
+
+    document.getElementById("contacto").addEventListener("click", function () {
+        alert("Gracias por visitar la sección de contacto.");
+    });
+
+});
